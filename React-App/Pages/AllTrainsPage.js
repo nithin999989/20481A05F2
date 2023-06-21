976e85c9312b418ffae46f1d56540bb06b151a9e
M N S R NITHIN <82808965+nithin999989@users.noreply.github.com>
2023-06-21 12:55:22 +0530

    Create AllTrainsPage.js

---
+++ React-App/Pages/AllTrainsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrainList from '../components/TrainList';
+
+const AllTrainsPage = () => {
+  return (
+    <div>
+      <h1>All Trains Page</h1>
+      <TrainList />
+    </div>
+  );
+};
+
+export default AllTrainsPage;
